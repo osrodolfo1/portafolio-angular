dafac98250cd068a8809cc43a79c331daf717483
OscarEstrada <osrodolfo2@gmail.com>
2020-11-16 17:12:40 -0600

    agregando template de portafolio con menu y footer como componentes compartidos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { PortafolioComponent } from './paginas/portafolio/portafolio.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule
